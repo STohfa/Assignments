22c7966c25796412354533f8cd988f7d83ab6559
STohfa <fsamihatohfa@gmail.com>
2022-03-23 22:47:26 -0600

    prime solution is done

--- primeNumber/prime.cpp
+++ primeNumber/prime.cpp
@@ -3,9 +3,34 @@
 
 using namespace std;
 
+bool isPrime(int num){
+    if(num <= 1){
+        return false;
+    }
+    if(num == 2 || num == 3){
+        return true;
+    }
+    if(num % 2 == 0){
+        return false;
+    }
+    int i;
+    for(i = 3; i * i <= num; i = i + 2){
+        if(num % i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
 int main()
 {
+    int num;
 
-    cout << "Hello World";
+    cin >> num;
+
+    bool res = isPrime(num);
+
+    cout << res;
+
     return 0;
 }
